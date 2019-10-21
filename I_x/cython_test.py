faa81d8877d5bbc326fdebf21d4ea5135f6bbc0d
Chen Cai <agoodweathercc>
2019-10-20 20:30:15 -0400

    speed up bfs with success

--- I_x/cython_test.py
+++ I_x/cython_test.py
@@ -8,6 +8,8 @@ from example import primes
 from time import time
 import sys
 from helper.format import precision_format as pf
+from bfs_faster import bfs_faster, bfs_faster2
+
 
 def bfs_fast_python(g, source):
     iter = nx.bfs_successors(g, source)
@@ -60,10 +62,38 @@ def get_iter(g, source, print_ = False, nx_flag = True):
         print(f'get_iter for graph of size {len(g)}/{len(g.edges())} takes {pf(t1-t0, 3)}')
     return iter_array, t1 - t0
 
+def bfs_successors(G, source, depth_limit=None):
+    for p, c in nx.bfs_edges(G, source, depth_limit=depth_limit):
+        yield (p, c)
+
+def get_iter(g, source, print_ = False, nx_flag = True):
+    # get iter_array efficiently
+    t0 = time()
+    if nx_flag:
+        iter = list(nx.bfs_successors(g, source))
+        iter_list = [[v] + nbrs for v, nbrs in iter]
+        iter_array = np.array(list(itertools.zip_longest(*iter_list, fillvalue=0))).T
+
+        iter_array = np.concatenate([iter_array, np.array(list(map(len, iter_list))).reshape(-1,1)], axis=1) # every row is of form [512 474   0 ...   0   0   2]
+        iter_array = iter_array.astype(int)
+    else:
+        iter_array = np.array(list((bfs_successors(g, source))))
+
+    t1 = time()
+    if print_:
+        print(iter_array)
+        print(f'get_iter for graph of size {len(g)}/{len(g.edges())} takes {pf(t1-t0, 3)}')
+    return iter_array, t1 - t0
+
+def get_iter_fast(g, source):
+    iter_array = np.array(list((bfs_successors(g, source))))
+    return iter_array, 0
+
+
 
 if __name__ == '__main__':
-
-    n = 2000
+    np.random.seed(42)
+    n =  2000
     n_iter = n
     G = nx.random_tree(n, seed=42) # nx.random_geometric_graph(100, 0.1)
     assert nx.is_connected(G)
@@ -76,6 +106,54 @@ if __name__ == '__main__':
     #     t += t_
     # print(f'total get_iter takes {t}')
     # sys.exit()
+
+    attr_dict = nx.get_edge_attributes(G, 'weight')
+    attr_arr = np.zeros((n, n))
+    for k, v in attr_dict.items():
+        attr_arr[k[0]][k[1]] = v
+
+    t1 = time()
+    for source in range(n_iter):
+        ultra_dict = bfs_fast_python(G, source)
+        ultra_array_ = [0] * len(ultra_dict)
+        for k, v in ultra_dict.items():
+            ultra_array_[k] = v
+
+        iter_array2, _ = get_iter_fast(G, source)
+        ultra_array2 = bfs_faster2(attr_arr, iter_array2, n)
+        assert (ultra_array_ == ultra_array2).all()
+
+    t2 = time()
+    print('*'*150)
+    sys.exit()
+
+
+    t1_, t2_ = 0, 0
+    for source in range(n_iter):
+        # iter_array1, _ = get_iter(G, source, print_ = False, nx_flag = True)
+        # iter_array2, _ = get_iter(G, source, print_ = False, nx_flag = False)
+
+        _t1 = time()
+        iter_array2, _ = get_iter_fast(G, source)
+        _t2 = time()
+
+        # ultra_array1 = bfs_faster(attr_arr, iter_array1, n) #todo: array1 has some bugs
+        ultra_array2 = bfs_faster2(attr_arr, iter_array2, n)
+
+        # print(ultra_array2)
+        # try:
+        #     assert (ultra_array1 == ultra_array2).all()
+        # except:
+        #     print(ultra_array1)
+        #     print(ultra_array2)
+        #     print('-'*50)
+
+        _t3 = time()
+
+        t1_ += (_t2 -_t1)
+        t2_ += (_t3 -_t2)
+
+    sys.exit(f'python/bfs_faster takes {(pf(t2 - t1, 3))}/{(pf(t1_, 3))}+{(pf(t2_, 3))}')
 
 
     t0 = time()
@@ -95,36 +173,6 @@ if __name__ == '__main__':
         bfs_fast_complied2(G, source, iter)
     t3 = time()
 
-    from bfs_faster import bfs_faster
-
-    attr_dict = nx.get_edge_attributes(G, 'weight')
-    attr_arr = np.zeros((n, n))
-    for k, v in attr_dict.items():
-        attr_arr[k[0]][k[1]] = v
-
-    t = 0
-    for source in range(n_iter):
-        _t1 = time()
-        iter = list(nx.bfs_successors(G, source))
-        iter_list = [[v] + nbrs for v, nbrs in iter]
-        # iter_array = np.zeros([len(iter_list), 1 + len(max(iter_list, key=lambda x: len(x)))])
-        #
-        # for i, j in enumerate(iter_list):
-        #     iter_array[i][0:len(j)] = j
-        #     iter_array[i][-1] = len(j)
-        iter_array = np.array(list(itertools.zip_longest(*iter_list, fillvalue=0))).T
-
-        iter_array = np.concatenate([iter_array, np.array(list(map(len, iter_list))).reshape(-1,1)], axis=1) # every row is of form [512 474   0 ...   0   0   2]
-        iter_array = iter_array.astype(int)
-
-        # _t1 = time()
-        bfs_faster(attr_arr, iter_array, n)
-        _t2 = time()
-
-        t += (_t2-_t1)
-    print(f'python/bfs_faster takes {(pf(t2 - t1, 3))}/{(pf(t, 3))}')
-
-    sys.exit()
 
 
     attr_dict = nx.get_edge_attributes(G, 'weight')
